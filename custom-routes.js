231d2ddcd156aa885313bec7fe95ae8871893483
Daniel Alves <daniel.xcoders@gmail.com>
2024-01-10 21:49:55 -0300

    feat: change all routes

---
+++ custom-routes.js
@@ -0,0 +1,72 @@
+const fs = require('fs');
+const path = require('path');
+
+module.exports = (req, res, next) => {
+  const dbFilePath = path.join(__dirname, 'db.json');
+  const dbData = JSON.parse(fs.readFileSync(dbFilePath, 'utf8'));
+
+  if (req.path === '/groups' && req.method === 'GET') {
+    const transformedData = transformDataForGroups(dbData.grupos);
+    res.json(transformedData);
+  } else if (req.path.startsWith('/groups/') && req.method === 'GET') {
+    const groupId = req.path.split('/')[2];
+    const foundGroup = dbData.grupos.find((group) => group.id === groupId);
+
+    if (foundGroup) {
+      const transformedData = transformGroup(
+        foundGroup,
+        dbData.grupos,
+        dbData.usuarios
+      );
+      res.json(transformedData);
+    } else {
+      res.status(404).send('Group not found');
+    }
+  } else {
+    next();
+  }
+};
+
+function transformDataForGroups(groups) {
+  const buildGroupHierarchy = (group) => ({
+    key: group.id,
+    groupCode: group.groupCode,
+    groupName: group.groupName,
+    isActive: group.isActive,
+    usersCount: group.users.length,
+    children: groups
+      .filter((g) => g.parentKey === group.id)
+      .map(buildGroupHierarchy),
+  });
+
+  return groups.filter((group) => !group.parentKey).map(buildGroupHierarchy);
+}
+
+function transformGroup(group, groups, users) {
+  const mapUsers = (userIds) =>
+    userIds.map((userId) => {
+      const user = users.find((u) => u.id === userId);
+      if (user) {
+        const { id, name, email } = user;
+        return { key: id, name, email };
+      }
+      return null;
+    });
+
+  let parentGroupInfo = null;
+  if (group.parentKey) {
+    const parentGroup = groups.find((g) => g.id === group.parentKey);
+    if (parentGroup) {
+      parentGroupInfo = `${parentGroup.id} - ${parentGroup.groupName}`;
+    }
+  }
+
+  return {
+    key: group.id,
+    groupCode: group.groupCode,
+    groupName: group.groupName,
+    isActive: group.isActive,
+    users: mapUsers(group.users).filter((user) => user !== null),
+    parentGroup: parentGroupInfo,
+  };
+}
--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 // server.js
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('fakedb.json');
+const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
+const customRoutes = require('./custom-routes');
+
 server.use(middlewares);
+server.use(customRoutes);
 server.use('', router);
 server.listen(process.env.PORT || 5000, () => {
   console.log('JSON Server is running');
